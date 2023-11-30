fac005a5098a9a6e9450959e8a7e3a7472a4786b
Ayşenur Kurt <119353801+aysenurkurt22@users.noreply.github.com>
2023-11-30 22:10:09 +0300

    Add files via upload

---
+++ Otobüs Bilet Buton/Otobüs Bilet Buton/BusTicket.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Otobüs_Bilet_Buton
+{
+    partial class BusTicket
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BusTicket));
+            this.Koltuk1Button = new System.Windows.Forms.Button();
+            this.Koltuk8Button = new System.Windows.Forms.Button();
+            this.Koltuk7Button = new System.Windows.Forms.Button();
+            this.Koltuk5Button = new System.Windows.Forms.Button();
+            this.Koltuk4Button = new System.Windows.Forms.Button();
+            this.Koltuk2Button = new System.Windows.Forms.Button();
+            this.Koltuk9Button = new System.Windows.Forms.Button();
+            this.Koltuk6Button = new System.Windows.Forms.Button();
+            this.Koltuk3Button = new System.Windows.Forms.Button();
+            this.ödemeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Koltuk1Button
+            // 
+            this.Koltuk1Button.BackColor = System.Drawing.Color.Linen;
+            this.Koltuk1Button.Location = new System.Drawing.Point(27, 12);
+            this.Koltuk1Button.Name = "Koltuk1Button";
+            this.Koltuk1Button.Size = new System.Drawing.Size(64, 59);
+            this.Koltuk1Button.TabIndex = 2;
+            this.Koltuk1Button.Text = "1";
+            this.Koltuk1Button.UseVisualStyleBackColor = false;
+            this.Koltuk1Button.Click += new System.EventHandler(this.KoltukButton_Click);
+            // 
+            // Koltuk8Button
+            // 
+            this.Koltuk8Button.BackColor = System.Drawing.Color.Linen;
+            this.Koltuk8Button.Location = new System.Drawing.Point(109, 186);
+            this.Koltuk8Button.Name = "Koltuk8Button";
+            this.Koltuk8Button.Size = new System.Drawing.Size(64, 59);
+            this.Koltuk8Button.TabIndex = 3;
+            this.Koltuk8Button.Text = "8";
+            this.Koltuk8Button.UseVisualStyleBackColor = false;
+            this.Koltuk8Button.Click += new System.EventHandler(this.KoltukButton_Click);
+            // 
+            // Koltuk7Button
+            // 
+            this.Koltuk7Button.BackColor = System.Drawing.Color.Linen;
+            this.Koltuk7Button.Location = new System.Drawing.Point(27, 186);
+            this.Koltuk7Button.Name = "Koltuk7Button";
+            this.Koltuk7Button.Size = new System.Drawing.Size(64, 59);
+            this.Koltuk7Button.TabIndex = 4;
+            this.Koltuk7Button.Text = "7";
+            this.Koltuk7Button.UseVisualStyleBackColor = false;
+            this.Koltuk7Button.Click += new System.EventHandler(this.KoltukButton_Click);
+            // 
+            // Koltuk5Button
+            // 
+            this.Koltuk5Button.BackColor = System.Drawing.Color.Linen;
+            this.Koltuk5Button.Location = new System.Drawing.Point(109, 101);
+            this.Koltuk5Button.Name = "Koltuk5Button";
+            this.Koltuk5Button.Size = new System.Drawing.Size(64, 59);
+            this.Koltuk5Button.TabIndex = 5;
+            this.Koltuk5Button.Text = "5";
+            this.Koltuk5Button.UseVisualStyleBackColor = false;
+            this.Koltuk5Button.Click += new System.EventHandler(this.KoltukButton_Click);
+            // 
+            // Koltuk4Button
+            // 
+            this.Koltuk4Button.BackColor = System.Drawing.Color.Linen;
+            this.Koltuk4Button.Location = new System.Drawing.Point(27, 101);
+            this.Koltuk4Button.Name = "Koltuk4Button";
+            this.Koltuk4Button.Size = new System.Drawing.Size(64, 59);
+            this.Koltuk4Button.TabIndex = 6;
+            this.Koltuk4Button.Text = "4";
+            this.Koltuk4Button.UseVisualStyleBackColor = false;
+            this.Koltuk4Button.Click += new System.EventHandler(this.KoltukButton_Click);
+            // 
+            // Koltuk2Button
+            // 
+            this.Koltuk2Button.BackColor = System.Drawing.Color.Linen;
+            this.Koltuk2Button.Location = new System.Drawing.Point(109, 12);
+            this.Koltuk2Button.Name = "Koltuk2Button";
+            this.Koltuk2Button.Size = new System.Drawing.Size(64, 59);
+            this.Koltuk2Button.TabIndex = 7;
+            this.Koltuk2Button.Text = "2";
+            this.Koltuk2Button.UseVisualStyleBackColor = false;
+            this.Koltuk2Button.Click += new System.EventHandler(this.KoltukButton_Click);
+            // 
+            // Koltuk9Button
+            // 
+            this.Koltuk9Button.BackColor = System.Drawing.Color.Linen;
+            this.Koltuk9Button.Location = new System.Drawing.Point(265, 186);
+            this.Koltuk9Button.Name = "Koltuk9Button";
+            this.Koltuk9Button.Size = new System.Drawing.Size(64, 59);
+            this.Koltuk9Button.TabIndex = 8;
+            this.Koltuk9Button.Text = "9";
+            this.Koltuk9Button.UseVisualStyleBackColor = false;
+            this.Koltuk9Button.Click += new System.EventHandler(this.KoltukButton_Click);
+            // 
+            // Koltuk6Button
+            // 
+            this.Koltuk6Button.BackColor = System.Drawing.Color.Linen;
+            this.Koltuk6Button.Location = new System.Drawing.Point(265, 101);
+            this.Koltuk6Button.Name = "Koltuk6Button";
+            this.Koltuk6Button.Size = new System.Drawing.Size(64, 59);
+            this.Koltuk6Button.TabIndex = 9;
+            this.Koltuk6Button.Text = "6";
+            this.Koltuk6Button.UseVisualStyleBackColor = false;
+            this.Koltuk6Button.Click += new System.EventHandler(this.KoltukButton_Click);
+            // 
+            // Koltuk3Button
+            // 
+            this.Koltuk3Button.BackColor = System.Drawing.Color.Linen;
+            this.Koltuk3Button.Location = new System.Drawing.Point(265, 12);
+            this.Koltuk3Button.Name = "Koltuk3Button";
+            this.Koltuk3Button.Size = new System.Drawing.Size(64, 59);
+            this.Koltuk3Button.TabIndex = 10;
+            this.Koltuk3Button.Text = "3";
+            this.Koltuk3Button.UseVisualStyleBackColor = false;
+            this.Koltuk3Button.Click += new System.EventHandler(this.KoltukButton_Click);
+            // 
+            // ödemeButton
+            // 
+            this.ödemeButton.BackColor = System.Drawing.Color.Linen;
+            this.ödemeButton.Location = new System.Drawing.Point(43, 271);
+            this.ödemeButton.Name = "ödemeButton";
+            this.ödemeButton.Size = new System.Drawing.Size(272, 37);
+            this.ödemeButton.TabIndex = 11;
+            this.ödemeButton.Text = "Ödeme";
+            this.ödemeButton.UseVisualStyleBackColor = false;
+            this.ödemeButton.Click += new System.EventHandler(this.ödemeButton_Click);
+            // 
+            // BusTicket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(357, 340);
+            this.Controls.Add(this.ödemeButton);
+            this.Controls.Add(this.Koltuk3Button);
+            this.Controls.Add(this.Koltuk6Button);
+            this.Controls.Add(this.Koltuk9Button);
+            this.Controls.Add(this.Koltuk2Button);
+            this.Controls.Add(this.Koltuk4Button);
+            this.Controls.Add(this.Koltuk5Button);
+            this.Controls.Add(this.Koltuk7Button);
+            this.Controls.Add(this.Koltuk8Button);
+            this.Controls.Add(this.Koltuk1Button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "BusTicket";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bus Ticket App";
+            this.Load += new System.EventHandler(this.BusTicket_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Koltuk1Button;
+        private System.Windows.Forms.Button Koltuk8Button;
+        private System.Windows.Forms.Button Koltuk7Button;
+        private System.Windows.Forms.Button Koltuk5Button;
+        private System.Windows.Forms.Button Koltuk4Button;
+        private System.Windows.Forms.Button Koltuk2Button;
+        private System.Windows.Forms.Button Koltuk9Button;
+        private System.Windows.Forms.Button Koltuk6Button;
+        private System.Windows.Forms.Button Koltuk3Button;
+        private System.Windows.Forms.Button ödemeButton;
+    }
+}
+
---
+++ Otobüs Bilet Buton/Otobüs Bilet Buton/BusTicket.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Otobüs_Bilet_Buton
+{
+    public partial class BusTicket : Form
+    { 
+        bool[] seciliMi = new bool[9];
+        
+        
+        public BusTicket()
+        {
+            InitializeComponent();
+        }
+        private void KoltukButton_Click(object sender, EventArgs e)
+        {
+            Button koltukSecim = (Button)sender;
+                //hangi butona tıklandıysa o ismi verir
+            int koltukNo=Convert.ToInt32(koltukSecim.Text);
+
+            int sıraNo = koltukNo - 1; 
+           
+
+            if (seciliMi[sıraNo]) // if (seciliMi[sıraNo] == true)
+                {
+                Koltuk1Button.BackColor = Color.Linen;
+                seciliMi[sıraNo] = false;
+            }
+            else
+            {
+                koltukSecim.BackColor = Color.DarkRed;
+               
+                seciliMi[sıraNo] = true;
+               
+            }
+            
+            
+        }
+
+        private void ödemeButton_Click(object sender, EventArgs e)
+        {
+            int fiyat;
+            int toplamKoltuk = 0;
+            for(int i = 0; i < 9; i++) { 
+            if (seciliMi[i]) {
+                toplamKoltuk++; 
+            }
+            }
+            fiyat = toplamKoltuk * 150;
+            MessageBox.Show("Ödemeniz: " + fiyat + "TL'dir.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BusTicket_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
